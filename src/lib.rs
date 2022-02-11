68cf59b2828857878ea3d695da7539a3f81af003
AlexandruCihodaru <cihodar@amazon.com>
2022-02-11 17:46:52 +0200

    [vm-allocator]: Update error types
    Updated the Error enum definition and used thiserror crate.
    
    Signed-off-by: AlexandruCihodaru <cihodar@amazon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,80 +7,31 @@
 mod id_allocator;
 
 pub use crate::id_allocator::IdAllocator;
-use std::fmt;
 use std::result;
+use thiserror::Error;
 
 /// Error type for IdAllocator usage.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Error)]
 pub enum Error {
     /// All ids from the range specified are exhausted.
+    #[error("Id counter overflowed.")]
     Overflow,
     /// An id that is not part of the specified range was requested to be released.
+    #[error("Specified id: {0} is not in the range.")]
     OutOfRange(u32),
     /// An id that was already released was requested to be released.
+    #[error("Specified id: {0} is already released.")]
     AlreadyReleased(u32),
     /// An id  that was never allocated was requested to be released.
+    #[error("Specified id: {0} was never allocated, can't release it.")]
     NeverAllocated(u32),
     /// There are no more IDs available in the manage range
+    #[error("The requested resource is not available.")]
     ResourceExhausted,
     /// The range to manage is invalid.
-    InvalidRange(u32, u32),
+    #[error("The range specified: {0}-{1} is not valid.")]
+    InvalidRange(u64, u64),
 }
-
-impl std::error::Error for Error {}
 
 /// Wrapper over std::result::Result
 pub type Result = result::Result<u32, Error>;
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use Error::*;
-
-        match *self {
-            Overflow => write!(f, "Id counter overflowed."),
-            OutOfRange(id) => write!(f, "Specified id: {} is not in the range.", id),
-            ResourceExhausted => {
-                write!(f, "There are no more available ids in the specified range.")
-            }
-            AlreadyReleased(id) => write!(f, "Specified id: {} is already released.", id),
-            NeverAllocated(id) => write!(
-                f,
-                "Specified id: {} was never allocated, can't release it",
-                id
-            ),
-            InvalidRange(begin, end) => {
-                write!(f, "The range specified: {}-{} is not valid.", begin, end)
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Error as AllocError;
-
-    #[test]
-    fn test_error_messages() {
-        let err = AllocError::Overflow;
-        assert_eq!(format!("{}", err), "Id counter overflowed.");
-        let err = AllocError::OutOfRange(3);
-        assert_eq!(format!("{}", err), "Specified id: 3 is not in the range.");
-        let err = AllocError::AlreadyReleased(3);
-        assert_eq!(format!("{}", err), "Specified id: 3 is already released.");
-        let err = AllocError::NeverAllocated(3);
-        assert_eq!(
-            format!("{}", err),
-            "Specified id: 3 was never allocated, can't release it"
-        );
-        let err = AllocError::ResourceExhausted;
-        assert_eq!(
-            format!("{}", err),
-            "There are no more available ids in the specified range."
-        );
-        let err = AllocError::InvalidRange(23, 5);
-        assert_eq!(
-            format!("{}", err),
-            "The range specified: 23-5 is not valid."
-        );
-    }
-}
